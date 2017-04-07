3b6c3b3d02d96b4100bc4048b9fe6cf262756a52
Sergey Blednov <sergey.blednov@pixacore.com>
2017-04-07 09:23:52 -0400

    Inintial commit

---
+++ Assets/Entities/Enemy/Enemy.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	public GameObject projectile;
+	public float projectilSpeed = 5f;
+	public float firingRate = 0.2f;
+
+	private float health = 200f;
+
+	void OnTriggerEnter2D (Collider2D collider)
+	{
+		Projectile missile = collider.gameObject.GetComponent<Projectile> ();
+		if (missile) {
+			health -= missile.GetDamage();
+			missile.Hit ();
+			if (health <= 0) {
+//				Destroy (gameObject);
+			}
+		}
+	}
+
+	void Update () 
+	{
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			InvokeRepeating ("Fire", 0.000001f, firingRate);
+		} else if (Input.GetKeyUp (KeyCode.Space)) {
+			CancelInvoke("Fire");
+		}
+	}
+
+	void Fire() 
+	{
+		GameObject beam = Instantiate (projectile, transform.position, Quaternion.identity);
+		beam.GetComponent<Rigidbody2D> ().velocity = new Vector3 (0, projectilSpeed, 0);
+	}
+}
---
+++ Assets/Entities/Player/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public GameObject projectile;
+	public float projectilSpeed = 5f;
+	public float speed = 1f;
+	public float padding = 1f;
+	public float firingRate = 0.2f;
+
+	private float xmin;
+	private float xmax;
+
+	void Start ()
+	{
+		float distance = transform.position.z - Camera.main.transform.position.z;
+		Vector3 leftmost = Camera.main.ViewportToWorldPoint (new Vector3 (0, 0, distance));
+		Vector3 rightmost = Camera.main.ViewportToWorldPoint (new Vector3 (1, 0, distance));
+		xmin = leftmost.x + padding;
+		xmax = rightmost.x - padding;;
+	}
+
+	void Update () 
+	{
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			transform.position += Vector3.left * speed * Time.deltaTime;
+		} else if (Input.GetKey (KeyCode.RightArrow)) {
+			transform.position += Vector3.right * speed * Time.deltaTime;
+		} else if (Input.GetKeyDown (KeyCode.Space)) {
+			InvokeRepeating ("Fire", 0.000001f, firingRate);
+		} else if (Input.GetKeyUp (KeyCode.Space)) {
+			CancelInvoke("Fire");
+		}
+
+		transform.position = new Vector3 (Mathf.Clamp(transform.position.x, xmin, xmax), transform.position.y, transform.position.z);
+	}
+
+	void Fire() 
+	{
+		GameObject beam = Instantiate (projectile, transform.position, Quaternion.identity);
+		beam.GetComponent<Rigidbody2D> ().velocity = new Vector3 (0, projectilSpeed, 0);
+	}
+}
